3472025fe0e8992674550988ca5ed3709339c7ee
AndriySysoenko <gaskonets@gmail.com>
2024-12-17 19:52:02 +0200

    create hw4

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,4 +1,4 @@
-import { IUser } from "../interfaces/user.interface";
+import { IUser, IUserDto } from "../interfaces/user.interface";
 import { read, write } from "../services/fs.service";
 
 class UserRepository {
@@ -17,6 +17,29 @@ class UserRepository {
     users.push(newUser);
     await write(users);
     return newUser;
+  }
+
+  public async getById(userId: number): Promise<IUser> {
+    const users = await read();
+    return users.find((user) => user.id === userId);
+  }
+
+  public async updateById(userId: number, dto: IUserDto): Promise<IUser> {
+    const users = await read();
+    const index = users.findIndex((user) => user.id === Number(userId));
+    const user = users[index];
+    user.name = dto.name;
+    user.email = dto.email;
+    user.password = dto.password;
+    await write(users);
+    return user;
+  }
+
+  public async deleteById(userId: number): Promise<void> {
+    const users = await read();
+    const index = users.findIndex((user) => user.id === Number(userId));
+    users.splice(index, 1);
+    await write(users);
   }
 }
 
